c31569eaf01fd7f7f47ec1136e25fe27260b1752
Endezzz <evgenijarcev247@gmail.com>
2026-07-03 00:06:34 +0300

    feat: finished implementing several classes and their rendering

--- src/src.cpp
+++ src/src.cpp
@@ -816,3 +816,501 @@ void ThirdLevelWindow :: Hide() {
     DeleteObject(background);
     DeleteObject(circuit);
 }
+
+
+//-------------------[ A B C   O B J E C T ]--------------------//
+ABC_Object :: ABC_Object(int InitX, int InitY) : Point(InitX, InitY) {} 
+
+ABC_Object :: ~ABC_Object() {}
+
+bool ABC_Object :: isActive() {
+    return active;
+}
+
+
+//---------------------[ R O C K E T ]--------------------------//
+Rocket :: Rocket(int InitX, int InitY) : ABC_Object(InitX, InitY) {}
+
+Rocket :: ~Rocket() {}
+
+int Rocket :: GetDerection() {
+    return derection;
+}
+
+void Rocket :: SetDerection(int NewDerection) {
+    derection = NewDerection;
+}
+
+void Rocket :: Show() {
+    HBRUSH redBrush = CreateSolidBrush(RGB(255,0,0));
+    HPEN blackPen = CreatePen(PS_SOLID, 3, RGB(0,0,0));
+    HBRUSH yellowBrush = CreateSolidBrush(RGB(255,210,0));
+
+    SelectObject(hdc, redBrush);
+    SelectObject(hdc, blackPen);
+
+    POINT poly[3];
+    BYTE polytype[3];
+
+    POINT polyFire[7];
+    BYTE polytypeFire[7];
+
+    if(derection == 0) {
+        Rectangle(hdc, x-20, y-10, x+20, y+10);
+
+        BeginPath(hdc);
+        poly[0].x = x+20;
+        poly[0].y = y+12;
+        poly[1].x = x+20;
+        poly[1].y = y-12;
+        poly[2].x = x+40;
+        poly[2].y = y;
+        polytype[0] = PT_MOVETO;
+        polytype[1] = PT_LINETO;
+        polytype[2] = PT_LINETO;
+        PolyDraw(hdc, poly, polytype, 3);
+        EndPath(hdc);
+        FillPath(hdc);
+
+        SelectObject(hdc, yellowBrush);
+        BeginPath(hdc);
+        polyFire[0].x = x-21;
+        polyFire[0].y = y-5;
+        polyFire[1].x = x-21;
+        polyFire[1].y = y+5;
+        polyFire[2].x = x-30;
+        polyFire[2].y = y+9;
+        polyFire[3].x = x-24;
+        polyFire[3].y = y+3;
+        polyFire[4].x = x-32;
+        polyFire[4].y = y;
+        polyFire[5].x = x-24;
+        polyFire[5].y = y-3;
+        polyFire[6].x = x-30;
+        polyFire[6].y = y-9;
+        polytypeFire[0] = PT_MOVETO;
+        polytypeFire[1] = PT_LINETO;
+        polytypeFire[2] = PT_LINETO;
+        polytypeFire[3] = PT_LINETO;
+        polytypeFire[4] = PT_LINETO;
+        polytypeFire[5] = PT_LINETO;
+        polytypeFire[6] = PT_LINETO;
+        PolyDraw(hdc, polyFire, polytypeFire, 7);
+        EndPath(hdc);
+        FillPath(hdc);
+    }
+
+    if(derection == 1) {
+        Rectangle(hdc, x-20, y-10, x+20, y+10);
+
+        BeginPath(hdc);
+        poly[0].x = x-20;
+        poly[0].y = y+12;
+        poly[1].x = x-20;
+        poly[1].y = y-12;
+        poly[2].x = x-40;
+        poly[2].y = y;
+        polytype[0] = PT_MOVETO;
+        polytype[1] = PT_LINETO;
+        polytype[2] = PT_LINETO;
+        PolyDraw(hdc, poly, polytype, 3);
+        EndPath(hdc);
+        FillPath(hdc);
+
+        SelectObject(hdc, yellowBrush);
+        BeginPath(hdc);
+        polyFire[0].x = x+21;
+        polyFire[0].y = y-5;
+        polyFire[1].x = x+21;
+        polyFire[1].y = y+5;
+        polyFire[2].x = x+30;
+        polyFire[2].y = y+9;
+        polyFire[3].x = x+24;
+        polyFire[3].y = y+3;
+        polyFire[4].x = x+32;
+        polyFire[4].y = y;
+        polyFire[5].x = x+24;
+        polyFire[5].y = y-3;
+        polyFire[6].x = x+30;
+        polyFire[6].y = y-9;
+        polytypeFire[0] = PT_MOVETO;
+        polytypeFire[1] = PT_LINETO;
+        polytypeFire[2] = PT_LINETO;
+        polytypeFire[3] = PT_LINETO;
+        polytypeFire[4] = PT_LINETO;
+        polytypeFire[5] = PT_LINETO;
+        polytypeFire[6] = PT_LINETO;
+        PolyDraw(hdc, polyFire, polytypeFire, 7);
+        EndPath(hdc);
+        FillPath(hdc);
+    }
+
+    DeleteObject(redBrush);
+    DeleteObject(blackPen);
+    DeleteObject(yellowBrush);
+}
+
+void Rocket :: Hide() {
+    HBRUSH blackBrush = CreateSolidBrush(RGB(0,0,0));
+    HPEN blackPen = CreatePen(PS_SOLID, 3, RGB(0,0,0));
+
+    SelectObject(hdc, blackBrush);
+    SelectObject(hdc, blackPen);
+
+    POINT poly[3];
+    BYTE polytype[3];
+
+    POINT polyFire[7];
+    BYTE polytypeFire[7];
+
+    if(derection == 0) {
+        Rectangle(hdc, x-20, y-10, x+20, y+10);
+
+        BeginPath(hdc);
+        poly[0].x = x+20;
+        poly[0].y = y+12;
+        poly[1].x = x+20;
+        poly[1].y = y-12;
+        poly[2].x = x+40;
+        poly[2].y = y;
+        polytype[0] = PT_MOVETO;
+        polytype[1] = PT_LINETO;
+        polytype[2] = PT_LINETO;
+        PolyDraw(hdc, poly, polytype, 3);
+        EndPath(hdc);
+        FillPath(hdc);
+
+        BeginPath(hdc);
+        polyFire[0].x = x-21;
+        polyFire[0].y = y-5;
+        polyFire[1].x = x-21;
+        polyFire[1].y = y+5;
+        polyFire[2].x = x-30;
+        polyFire[2].y = y+9;
+        polyFire[3].x = x-24;
+        polyFire[3].y = y+3;
+        polyFire[4].x = x-32;
+        polyFire[4].y = y;
+        polyFire[5].x = x-24;
+        polyFire[5].y = y-3;
+        polyFire[6].x = x-30;
+        polyFire[6].y = y-9;
+        polytypeFire[0] = PT_MOVETO;
+        polytypeFire[1] = PT_LINETO;
+        polytypeFire[2] = PT_LINETO;
+        polytypeFire[3] = PT_LINETO;
+        polytypeFire[4] = PT_LINETO;
+        polytypeFire[5] = PT_LINETO;
+        polytypeFire[6] = PT_LINETO;
+        PolyDraw(hdc, polyFire, polytypeFire, 7);
+        EndPath(hdc);
+        FillPath(hdc);
+    }
+
+    if(derection == 1) {
+        Rectangle(hdc, x-20, y-10, x+20, y+10);
+
+        BeginPath(hdc);
+        poly[0].x = x-20;
+        poly[0].y = y+12;
+        poly[1].x = x-20;
+        poly[1].y = y-12;
+        poly[2].x = x-40;
+        poly[2].y = y;
+        polytype[0] = PT_MOVETO;
+        polytype[1] = PT_LINETO;
+        polytype[2] = PT_LINETO;
+        PolyDraw(hdc, poly, polytype, 3);
+        EndPath(hdc);
+        FillPath(hdc);
+
+        BeginPath(hdc);
+        polyFire[0].x = x+21;
+        polyFire[0].y = y-5;
+        polyFire[1].x = x+21;
+        polyFire[1].y = y+5;
+        polyFire[2].x = x+30;
+        polyFire[2].y = y+9;
+        polyFire[3].x = x+24;
+        polyFire[3].y = y+3;
+        polyFire[4].x = x+32;
+        polyFire[4].y = y;
+        polyFire[5].x = x+24;
+        polyFire[5].y = y-3;
+        polyFire[6].x = x+30;
+        polyFire[6].y = y-9;
+        polytypeFire[0] = PT_MOVETO;
+        polytypeFire[1] = PT_LINETO;
+        polytypeFire[2] = PT_LINETO;
+        polytypeFire[3] = PT_LINETO;
+        polytypeFire[4] = PT_LINETO;
+        polytypeFire[5] = PT_LINETO;
+        polytypeFire[6] = PT_LINETO;
+        PolyDraw(hdc, polyFire, polytypeFire, 7);
+        EndPath(hdc);
+        FillPath(hdc);
+    }
+
+    DeleteObject(blackBrush);
+    DeleteObject(blackPen);
+}
+
+
+//------------------------[ C H A R G E ]----------------------//
+Charge :: Charge(int InitX, int InitY) : Rocket(InitX, InitY) {}
+
+Charge :: ~Charge() {}
+
+int Charge :: GetRadius() {
+    return radius;
+}
+
+void Charge :: SetRadius(int NewRadius) {
+    radius = NewRadius;
+}
+
+void Charge :: Show() {
+    HBRUSH yellowBrush = CreateSolidBrush(RGB(255,165,0));
+
+    SelectObject(hdc, yellowBrush);
+
+    int x1 = x - radius; 
+    int y1 = y - radius;
+    int x2 = x + radius;
+    int y2 = y + radius;
+
+    Ellipse(hdc, x1, y1, x2, y2);
+
+    DeleteObject(yellowBrush);
+}
+
+void Charge :: Hide() {
+    HBRUSH blackBrush = CreateSolidBrush(RGB(0,0,0));
+
+    SelectObject(hdc, blackBrush);
+
+    int x1 = x - radius; 
+    int y1 = y - radius;
+    int x2 = x + radius;
+    int y2 = y + radius;
+
+    Ellipse(hdc, x1, y1, x2, y2);
+
+    DeleteObject(blackBrush);
+}
+
+
+//-------------------------[ M E T E O R ]--------------------//
+Meteor :: Meteor(int InitX, int InitY) : Charge(InitX, InitY) {}
+
+Meteor :: ~Meteor() {}
+
+void Meteor :: Show() {
+    HBRUSH grayBrush = CreateSolidBrush(RGB(50,50,50));
+    HBRUSH blackBrush = CreateSolidBrush(RGB(0,0,0));
+    HPEN grayPen = CreatePen(PS_SOLID, 2, RGB(50,50,50));
+    HPEN blackPen = CreatePen(PS_SOLID, 2, RGB(0,0,0));
+    HBRUSH yellowBrush = CreateSolidBrush(RGB(255,200,0));
+    HPEN yellowPen = CreatePen(PS_SOLID, 2, RGB(255,200,0));
+
+    int x1 = x - radius;
+    int y1 = y - radius;
+    int x2 = x + radius;
+    int y2 = y + radius;
+
+    SelectObject(hdc, grayBrush);
+    SelectObject(hdc, grayPen);
+
+    Ellipse(hdc, x1, y1, x2, y2);
+
+    SelectObject(hdc, blackBrush);
+    SelectObject(hdc, blackPen);
+
+    x1 = x - radius/3 - 13;
+    y1 = y - radius/3 - 13;
+    x2 = x + radius/3 - 13;
+    y2 = y + radius/3 - 13;
+
+    Ellipse(hdc, x1, y1, x2, y2);
+
+    x1 = x - radius/4 + 12;
+    y1 = y - radius/4 - 3;
+    x2 = x + radius/4 + 12;
+    y2 = y + radius/4 - 3;
+
+    Ellipse(hdc, x1, y1, x2, y2);
+
+    x1 = x - radius/6;
+    y1 = y - radius/6 + 15;
+    x2 = x + radius/6;
+    y2 = y + radius/6 + 15;
+
+    Ellipse(hdc, x1, y1, x2, y2);
+
+    SelectObject(hdc, yellowBrush);
+    SelectObject(hdc, yellowPen);
+
+    POINT poly[4];
+    BYTE polytype[4] = { PT_MOVETO, PT_LINETO, PT_LINETO, PT_LINETO };
+    
+    if(GetDerection() == 0) {
+        int flameWidth = 6;
+        int flameHeight = 6;
+
+        auto DrawFlame = [&](int cx, int cy) {
+            poly[0] = { cx, cy - flameHeight };
+            poly[1] = { cx + flameWidth, cy };
+            poly[2] = { cx, cy + flameHeight };
+            poly[3] = { cx - flameWidth, cy };
+            
+            BeginPath(hdc);
+            PolyDraw(hdc, poly, polytype, 4);
+            EndPath(hdc);
+            FillPath(hdc);
+        };
+
+        int diagonalOffsetX = -10;  
+        int diagonalOffsetY = 10;   
+
+        int startX = x - radius - 5;
+        int startY = y + radius + 5;
+
+        DrawFlame(startX+1, startY-5);                            
+        DrawFlame(startX + diagonalOffsetX+20, startY + diagonalOffsetY+10);     
+        DrawFlame(startX + diagonalOffsetX*2, startY + diagonalOffsetY*2-5); 
+    }
+
+    if(GetDerection() == 1) {
+        int flameWidth = 6;
+        int flameHeight = 6;
+
+        auto DrawFlame = [&](int cx, int cy) {
+            poly[0] = { cx, cy - flameHeight };
+            poly[1] = { cx + flameWidth, cy };
+            poly[2] = { cx, cy + flameHeight };
+            poly[3] = { cx - flameWidth, cy };
+            
+            BeginPath(hdc);
+            PolyDraw(hdc, poly, polytype, 4);
+            EndPath(hdc);
+            FillPath(hdc);
+        };
+
+        int diagonalOffsetX = 10;   
+        int diagonalOffsetY = -10;
+
+        int startX = x + radius + 15;   
+        int startY = y - radius - 15;   
+
+        DrawFlame(startX-10, startY+5);                                          
+        DrawFlame(startX + diagonalOffsetX + 16, startY + diagonalOffsetY+20);      
+        DrawFlame(startX + diagonalOffsetX*2, startY + diagonalOffsetY*2);  
+    }
+
+
+    DeleteObject(grayBrush);
+    DeleteObject(blackBrush);
+    DeleteObject(blackPen);
+    DeleteObject(grayPen);
+    DeleteObject(yellowPen);
+    DeleteObject(yellowBrush);
+}
+
+void Meteor :: Hide() {
+    HBRUSH blackBrush = CreateSolidBrush(RGB(0,0,0));
+    HPEN blackPen = CreatePen(PS_SOLID, 2, RGB(0,0,0));
+
+    SelectObject(hdc, blackBrush);
+    SelectObject(hdc, blackPen);
+
+    int x1 = x - radius;
+    int y1 = y - radius;
+    int x2 = x + radius;
+    int y2 = y + radius;
+
+    Ellipse(hdc, x1, y1, x2, y2);
+
+    x1 = x - radius/3 - 13;
+    y1 = y - radius/3 - 13;
+    x2 = x + radius/3 - 13;
+    y2 = y + radius/3 - 13;
+
+    Ellipse(hdc, x1, y1, x2, y2);
+
+    x1 = x - radius/4 + 12;
+    y1 = y - radius/4 - 3;
+    x2 = x + radius/4 + 12;
+    y2 = y + radius/4 - 3;
+
+    Ellipse(hdc, x1, y1, x2, y2);
+
+    x1 = x - radius/6;
+    y1 = y - radius/6 + 15;
+    x2 = x + radius/6;
+    y2 = y + radius/6 + 15;
+
+    Ellipse(hdc, x1, y1, x2, y2);
+
+    POINT poly[4];
+    BYTE polytype[4] = { PT_MOVETO, PT_LINETO, PT_LINETO, PT_LINETO };
+    
+    if(GetDerection() == 0) {
+        int flameWidth = 6;
+        int flameHeight = 6;
+
+        auto DrawFlame = [&](int cx, int cy) {
+            poly[0] = { cx, cy - flameHeight };
+            poly[1] = { cx + flameWidth, cy };
+            poly[2] = { cx, cy + flameHeight };
+            poly[3] = { cx - flameWidth, cy };
+            
+            BeginPath(hdc);
+            PolyDraw(hdc, poly, polytype, 4);
+            EndPath(hdc);
+            FillPath(hdc);
+        };
+
+        int diagonalOffsetX = -10;  
+        int diagonalOffsetY = 10;   
+
+        int startX = x - radius - 5;
+        int startY = y + radius + 5;
+
+        DrawFlame(startX+1, startY-5);                            
+        DrawFlame(startX + diagonalOffsetX+20, startY + diagonalOffsetY+10);     
+        DrawFlame(startX + diagonalOffsetX*2, startY + diagonalOffsetY*2-5); 
+    }
+
+    if(GetDerection() == 1) {
+        int flameWidth = 6;
+        int flameHeight = 6;
+
+        auto DrawFlame = [&](int cx, int cy) {
+            poly[0] = { cx, cy - flameHeight };
+            poly[1] = { cx + flameWidth, cy };
+            poly[2] = { cx, cy + flameHeight };
+            poly[3] = { cx - flameWidth, cy };
+            
+            BeginPath(hdc);
+            PolyDraw(hdc, poly, polytype, 4);
+            EndPath(hdc);
+            FillPath(hdc);
+        };
+
+        int diagonalOffsetX = 10;   
+        int diagonalOffsetY = -10;
+
+        int startX = x + radius + 15;   
+        int startY = y - radius - 15;   
+
+        DrawFlame(startX-10, startY+5);                                          
+        DrawFlame(startX + diagonalOffsetX + 16, startY + diagonalOffsetY+20);      
+        DrawFlame(startX + diagonalOffsetX*2, startY + diagonalOffsetY*2);  
+    }
+    
+    DeleteObject(blackBrush);
+    DeleteObject(blackPen);
+}
+
+
+//-------------------[ D A R K   H O L E ]--------------------//
